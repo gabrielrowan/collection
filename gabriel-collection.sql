3cd13da1e12ed9b583fc3b46557de93c622403a0
Gabriel Rowan <gabrielrowan82@gmail.com>
2022-02-07 11:57:17 +0000

    structure of database done

---
+++ gabriel-collection.sql
@@ -0,0 +1,44 @@
+# ************************************************************
+# Sequel Ace SQL dump
+# Version 20025
+#
+# https://sequel-ace.com/
+# https://github.com/Sequel-Ace/Sequel-Ace
+#
+# Host: 127.0.0.1 (MySQL 5.7.37)
+# Database: gabriel-collection
+# Generation Time: 2022-02-07 11:53:45 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE='NO_AUTO_VALUE_ON_ZERO', SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table cat_types
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `cat_types`;
+
+CREATE TABLE `cat_types` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `Breed Name` varchar(255) NOT NULL DEFAULT '',
+  `Country of Origin` varchar(255) NOT NULL DEFAULT '',
+  `Fluffiness Rating` enum('1','2','3','4','5') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
