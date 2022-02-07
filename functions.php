4feda8f4f65e23dfc5f7991e2d63bc3806e4de61
Gabriel Rowan <gabrielrowan82@gmail.com>
2022-02-07 15:54:36 +0000

    added docblocks and typehinting

--- functions.php
+++ functions.php
@@ -1,12 +1,23 @@
 <?php
 
-function get_db() {
+/**
+ * Description: Connects the php file to the database
+ * @return object
+ */
+
+function get_db(): object {
 
     return $db = new PDO('mysql:host=db; dbname=gabriel-collection', 'root', 'password');
 
 }
 
-function retrieve_db_items($db) {
+/**
+ * Description: Returns queried data from the database
+ * @param object $db - The database you are querying
+ * @return array - All queried data will be placed in an array
+ */
+
+function retrieve_db_items(object $db): array {
 
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $query = $db->prepare("SELECT `breed_name`, `country_of_origin`, `fluffiness_rating`, `image` FROM `cat_types`;");
