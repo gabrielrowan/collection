b84a1d690b11a5d4f9d8cc75c3d2c1cff178e31b
Gabriel Rowan <gabrielrowan82@gmail.com>
2022-02-08 16:55:37 +0000

    last commit of the day

--- functions.php
+++ functions.php
@@ -1,55 +1,52 @@
 <?php
 
 /**
- * Description: Connects the php file to the database
+ * Connects the php file to the database
+ *
  * @return object
  */
-
-function get_db(): object {
+function get_db(): PDO {
 
     return new PDO('mysql:host=db; dbname=gabriel-collection', 'root', 'password');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 }
 
 /**
- * Description: Returns queried data from the database
+ * Returns queried data from the database
+ *
  * @param object $db - The database you are querying
- * @return array - All queried data will be placed in an array
+ *
+ * @return array - Returns an array of cat data from the database
  */
+function retrieve_db_items(PDO $db): array {
 
-function retrieve_db_items(object $db): array {
-
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $query = $db->prepare("SELECT `breed_name`, `country_of_origin`, `fluffiness_rating`, `image` FROM `cat_types`;");
     $query->execute();
     return $query->fetchAll();
 
 }
 
-
 /**
- * Description: Iterates through cat breeds array and prints out HTML formatted results per cat breed
+ * Iterates through cat breeds array and prints out HTML formatted results per cat breed
+ *
  * @param array $catBreeds
+ *
  * @return string Outputs a concatenated list of HTML formatted results per cat breed
  */
-
-
-
 function printCatBreed(array $catBreeds): string {
     if (!count($catBreeds)) {
-        return "No cat informative given!";
+        return "No cat information given!";
     }
     $result = "";
     foreach ($catBreeds as $catBreed) {
-        $result .= '<div class="breed">' .
-            '<div class = "name">' . $catBreed['breed_name'] . '</div>' .
-            '<div class = "countryOfOrigin">' . "Country of Origin: " . $catBreed['country_of_origin'] . '</div>' .
-            '<div class = "fluffiness">' . "Fluffiness Rating: " . $catBreed['fluffiness_rating'] . '</div>' .
-            '<div class = "photo">' . "<img src='" . $catBreed['image'] . "'/>" . '</div>' .
-        '</div>';
+        $result .= "<div class='breed'> 
+            <div class = 'name'> $catBreed[breed_name] </div> 
+            <div class = 'countryOfOrigin'>Country of Origin: $catBreed[country_of_origin] </div>
+            <div class = 'fluffiness'>Fluffiness Rating: $catBreed[fluffiness_rating] </div>
+            <div class = 'photo'> <img alt='Cat Photo' src='$catBreed[image]'/> </div>
+        </div>";
     }
 
     return $result;
 };
-
-
